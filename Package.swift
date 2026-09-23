596342201686454b0dcf9427493c1c7dc87b2061
蒋潇远 <jiangxiaoyuan@jiangxiaoyuandeMacBook-Air.local>
2026-09-23 13:17:36 +0800

    Fix SwiftPM manifest: set defaultLocalization for en.lproj/zh-Hans.lproj

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CodexGateway",
+    defaultLocalization: "en",
     platforms: [.macOS("26.0")],
     products: [
         .executable(
